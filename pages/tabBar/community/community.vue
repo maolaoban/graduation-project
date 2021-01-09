6dcd0529542304ccdda665983563c7cf2fb444f5
maolaoban <1393867317@qq.com>
2021-01-09 18:13:40 +0800

    社区

--- pages/tabBar/community/community.vue
+++ pages/tabBar/community/community.vue
@@ -1,6 +1,19 @@
 <template>
-	<view>
-		社区
+	<view class="community-container">
+		<view class="community-top">
+			<view class="top-post">
+				<view class="post-icon">置顶</view>
+				<text class="post-title">#官方# 社区须知！发帖必看！！！</text>
+			</view>
+			<view class="top-post">
+				<view class="post-icon">置顶</view>
+				<text class="post-title">#官方# 社区须知！！！</text>
+			</view>
+			<view class="top-post">
+				<view class="post-icon">置顶</view>
+				<text class="post-title">科技美学招聘，后期剪辑，等你来哦</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +30,34 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+.community-top{
+	width: 100%;
+	padding: 20rpx;
+	.top-post{
+		height: 80rpx;
+		padding-left: 10rpx;
+		padding-right: 10rpx;
+		display: flex;
+		align-items: center;
+		border-bottom: 0.5rpx solid rgba($color: $uni-text-color-inverse, $alpha: 0.5);
+		.post-icon{
+			width: 50rpx;
+			height:30rpx;
+			text-align: center;
+			color: $uni-text-color-inverse;
+			border-radius: 10rpx;
+			background-color: #ffaa00;
+			font-size: 20rpx;
+		}
+		.post-title{
+			color: $uni-text-color-inverse;
+			font-size: 28rpx;
+			margin-left: 20rpx;
+			overflow: hidden;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+		}
+	}
+}
 </style>
